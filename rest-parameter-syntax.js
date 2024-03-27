252c1351ce8dd63527c4a6c1ed6f35275d089db3
Alsona Natsi <alsona.naci1188@gmail.com>
2024-03-27 20:28:51 +0000

    Rest parameter syntax

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,25 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+//call the function using the number 1, 2, 3
+
+let sum = sumAll(1, 2, 3);
+console.log ("The sum is: ", sum);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6); // the sum1 is equal to sum2!!!
 
 // Function using ...rest
+
+const sumRest = (a, b , c, ...rest) => {
+    let sum = a+ b + c;
+    for(let i of rest){
+        sum += i;
+    }
+    return sum;
+};
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+
+console.log("The sum is: " , sum3);
